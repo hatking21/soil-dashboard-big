01ef8f348f0816d3da7d725501c179bef09bff0b
hatking21 <nathanbullis99@gmail.com>
2026-03-18 01:38:36 -0700

    Update soil_dashboard.py
    updated port

--- soil_dashboard.py
+++ soil_dashboard.py
@@ -229,4 +229,4 @@ def update_dashboard(n):
 threading.Thread(target=mqtt_thread, daemon=True).start()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8050)
+    app.run(host="0.0.0.0", port=10000)
